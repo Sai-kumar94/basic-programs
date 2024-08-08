df0e8fecc47b0c86feab79c2f5f250eed532f0b4
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-08-08 23:50:24 +0530

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,30 @@
+# normal program
+
+
+index_count=0
+words='abcde'
+for letter in words:
+    print(words[index_count])
+    index_count += 1
+
+
+# using enumerate built-in function
+
+
+
+words='abcde'
+for item in enumerate(words):
+    print(words)
+    print(item)
+
+
+# tuple unpacking
+
+
+words='abcde'
+for  index,letter in enumerate(words):
+    print(index)
+    print(letter)
+    print()
+
+
