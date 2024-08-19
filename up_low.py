f1880222200f52f2853d0fa42a358fc1090be2e4
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-08-19 12:44:37 +0530

    Update up_low.py

--- up_low.py
+++ up_low.py
@@ -15,3 +15,20 @@ def up_low(s):
     print(f'uppercase count : {uppercase}')
     print(f'lowercase count : {lowercase}  ')
 up_low('Hi,How are you?')
+
+# or
+
+def up_low(s):
+    d={'upper':0,'lower':0 }
+    for char in s:
+        if char.isupper():
+            d['upper']+=1
+        if char.islower():
+            d['lower']+=1
+        else:
+            pass
+    print(f'original string : {s}')
+    print(f'uppercase count : {d["upper"]}')
+    print(f'lowercase count : {d["lower"]}')
+up_low('Hi,How are you?')
+
