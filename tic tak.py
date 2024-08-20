5843146b2441f4597dfde92633ac9a8c449c5a10
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-08-20 15:23:22 +0530

    Create tic tak.py

---
+++ tic tak.py
@@ -0,0 +1,12 @@
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+row1=[' ',' ',' ']
+row2=[' ',' ',' ']
+row3=[' ',' ',' ']
+print(display(row1,row2,row3))
+row2[1]='X'
+row1[0]='X'
+row3[2]='X'
+print(display(row1,row2,row3))
