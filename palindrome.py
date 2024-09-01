5dbe681cc10ee1bab94850c4f63f0d304aac088c
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-09-01 17:22:18 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,4 +3,18 @@ def palindrome(s):
     s = s.replace(' ','')
     #check string is == reversed version of the string
     return s==s[::-1]
-print(palindrome('nurses run')) 
+print(palindrome('nurses run'))
+
+
+def is_palindrome(string):
+    string=string.lower()
+    return string==string[::-1]
+print(is_palindrome("madam"))
+
+
+def is_palindrome(string):
+    if string==string[::-1]:
+        print('it is a palindrome')
+    else:
+        print('not a palindrome')
+print((is_palindrome("madam")))
